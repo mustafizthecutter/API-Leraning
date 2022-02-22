befbd9cda0f32457f1801c21fe9f71355ef292db
Mustafizur Rahman <mustafizur232@gmail.com>
2022-02-22 17:37:23 +0600

    third-commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const shop = { name: 'dipur Dokan', address: 'mirpur', category: 'clothes shop', about: { constu: 'chiku', isExpensive: false } };
 const shopStringified = JSON.stringify(shop);
+const converted = JSON.parse(shopStringified);
+console.log(converted);
 console.log(shopStringified);
 console.log(shop);
