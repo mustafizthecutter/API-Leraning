df453315292618262e2b81161cad3080eae98491
Mustafizur Rahman <mustafizur232@gmail.com>
2022-02-25 00:29:50 +0600

    thirteen commit

--- js/mealdb.js
+++ js/mealdb.js
@@ -6,5 +6,25 @@ const loadFood = () => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputFieldValue}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.meals));
+        .then(data => displayFood(data.meals));
+}
+const displayFood = meals => {
+    const searchResult = document.getElementById('search-result');
+    meals.forEach(meal => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+                <div class="card h-100">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+         `;
+        searchResult.appendChild(div);
+    })
 }
