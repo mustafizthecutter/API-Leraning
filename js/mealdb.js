b9cce5700bf01a6a11170e41450e8b4877a06e0b
Mustafizur Rahman <mustafizur232@gmail.com>
2022-02-25 00:55:00 +0600

    twelve commit

--- js/mealdb.js
+++ js/mealdb.js
@@ -11,16 +11,16 @@ const loadFood = () => {
 const displayFood = meals => {
     const searchResult = document.getElementById('search-result');
     meals.forEach(meal => {
+
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card">
                 <div class="card h-100">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="${meal.strMealThumb}"class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
+                        <h5 class="card-title">${meal.strMeal}</h5>
+                        <p class="card-text">${meal.strInstructions.slice(0, 500)}</p>
                     </div>
                 </div>
             </div>
